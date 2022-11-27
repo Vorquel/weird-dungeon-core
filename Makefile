3fa606d06ec6116c7a329799f2f96da67eab3f82
Vorquel <git@vorquel.com>
2022-11-27 00:31:06 -0700

    WIP

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: build
 build: chapters/* jinja/* venv
-	rm site/*.html
+	rm site/*.html || true
 	./venv/bin/python build.py
-venv: requrements.txt
+venv: requirements.txt
 	python3 -m venv --clear venv
 	./venv/bin/pip install -r requirements.txt
--- build.py
+++ build.py
@@ -1,16 +1,30 @@
 from dataclasses import KW_ONLY, dataclass, field
+from itertools import count
+from os.path import isfile
 from jinja2 import Environment, FileSystemLoader
 
 @dataclass
 class Block:
     _ = KW_ONLY
-    cls = "txt"
-    lines = field(default_factory=list)
+    cls: str = "txt"
+    lines: list[str] = field(default_factory=list)
 
 def main():
-    env = Environment(keep_trailing_newline=True, loader=FileSystemLoader("jinja/"))
-    index = env.get_template("index.html")
-    chapter = env.get_template("chapter.html")
+    env = Environment(
+        keep_trailing_newline=True,
+        loader=FileSystemLoader("jinja/"),
+    )
+    home = env.get_template("index.html")
+    chap = env.get_template("chapter.html")
+    chapters = {}
+    for index in count(1):
+        filename = f"chapters/{index}.txt"
+        if not isfile(filename):
+            break
+        with open(filename) as file:
+            parts = file.read().split("\n\n")
+        chapters[f"chapter-1.html"] = parts
+    print(f"{chapters!r}")
 
-if name == "__name__":
+if __name__ == "__main__":
     main()

d7cf3beca23cdaff689e4d6937f1944bead77dd9
Vorquel <git@vorquel.com>
2022-11-27 01:15:15 -0700

    Get templates working

--- build.py
+++ build.py
@@ -11,6 +11,21 @@ class Block:
     cls: str = "txt"
     lines: list[str] = field(default_factory=list)
 
+def from_parts(parts):
+    out = []
+    for part in parts:
+        if part == "---":
+            out.append("<hr>")
+            continue
+        block = Block()
+        lines = part.split('\n')
+        if lines[0][0] == ".":
+            (cls, *lines) = lines
+            block.cls = cls[1:]
+        block.lines = lines
+        out.append(block)
+    return out
+
 def main():
     env = Environment(
         keep_trailing_newline=True,
@@ -25,12 +40,21 @@ def main():
             break
         with open(filename) as file:
             parts = file.read().split("\n\n")
-        chapters[f"chapter-1.html"] = parts
+        chapters[f"chapter-1.html"] = from_parts(parts)
     with open("site/index.html", "w") as file:
         file.write(home.render(
             chapters=[*chapters],
             title=TITLE,
         ))
+    for key, parts in chapters.items():
+        with open(f"site/{key}", "w") as file:
+            file.write(chap.render(
+                chapters=[*chapters],
+                title=TITLE,
+                parts=parts,
+                prev="index.html",
+                next="index.html",
+            ))
 
 if __name__ == "__main__":
     main()
